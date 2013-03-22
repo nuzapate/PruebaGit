6dab389a015de0455066a83d95bc58881a2c9d1c
nuzapate <vuplx_90@hotmail.com>
2013-03-22 20:30:57 +0100

    Commit inicial del proyecto.

---
+++ src/mipruebagit/PruebaGit.java
@@ -0,0 +1,13 @@
+package mipruebagit;
+
+public class PruebaGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("A ver si Git nos gusta");
+
+	}
+
+}
