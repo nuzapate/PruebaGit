d57b6d49f72fdb84df774174e33ed3c48b7e6e0a
nuzapate <vuplx_90@hotmail.com>
2013-04-12 21:50:25 +0300

    Update PruebaGit.java
    Modificación desde Github.

--- src/mipruebagit/PruebaGit.java
+++ src/mipruebagit/PruebaGit.java
@@ -6,7 +6,7 @@ public class PruebaGit {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Subversión, tú antes eras guay");
+		System.out.println("Subversión, tú antes eras guay.");
 		System.out.println("Cambio realizado desde Github, versión 2.");
 
 	}
