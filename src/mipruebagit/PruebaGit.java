54a0e6a89f3cc3e4e4c18a6d73e254df53d7fd6d
Carlos <Carlos@Carlos>
2013-04-20 13:01:19 +0200

    Añado cuarta linea

--- src/mipruebagit/PruebaGit.java
+++ src/mipruebagit/PruebaGit.java
@@ -6,9 +6,10 @@ public class PruebaGit {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Subversión, tú antes eras guay.");
-		System.out.println("Cambio realizado desde Github, versión 2.");
+		System.out.println("Subversion, tu antes eras guay.");
+		System.out.println("Cambio realizado desde Github, version 2.");
 		System.out.println("Cambio realizado desde Github, version 3.");
+		System.out.println("Cambio realizado desde Github, version 4.");
 
 	}
 
